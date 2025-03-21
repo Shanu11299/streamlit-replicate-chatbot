6524f82c84423e556c269f645c0b542718ab63a0
Shanu11299 <shanudesilva624@gmail.com>
2025-03-21 19:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 if not os.path.exists(nltk_data_path):
     os.makedirs(nltk_data_path)
 
-# Set the NLTK data path and download 'punkt'
+# Set the NLTK data path
 nltk.data.path.append(nltk_data_path)
-nltk.download('punkt', download_dir=nltk_data_path)
+
+# Manually check and download 'punkt' tokenizer
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama Newly 2 Chatbot")
